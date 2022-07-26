f8ed05fa171fd9d816f818772fec7f603d1035a0
Daniela Cortés <39687467+daniccast@users.noreply.github.com>
2022-07-25 20:39:18 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -46,6 +46,6 @@ setup(
     test_suite="tests",
     tests_require=TEST_REQUIREMENTS,
     url="https://github.com/victorcabello/pystandalonehtml",
-    version="0.1.10",
+    version="0.1.11",
     zip_safe=False,
 )
